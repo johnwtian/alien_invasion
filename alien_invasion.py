2d9aa5ff7aeaeabc7948e13582a68fc4cc724ef0
johnwtian <66046129+johnwtian@users.noreply.github.com>
2020-06-12 14:02:24 -0700

    Adding bullet functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 def run_game():
@@ -15,6 +16,9 @@ def run_game():
 	#Make a ship
 	ship = Ship(ai_settings, screen)
 
+	#Make a group to store bullets
+	bullets = Group()
+
 	#Set the background color.
 
 	bg_color = (230, 230, 230)
@@ -22,9 +26,10 @@ def run_game():
 	# Start the main loop for the game.
 	while True:
 
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 
