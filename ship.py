cb04b54e85fecc8ac10b8e29951e8c4e4aeef3d3
johnwtian <66046129+johnwtian@users.noreply.github.com>
2020-06-12 11:41:42 -0700

    Alien invasion update
    Added movement with edge limits on ship

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-	def __init__(self, screen):
+	def __init__(self, ai_settings, screen):
 		"""Initialize the ship and set its starting position"""
 		self.screen = screen
+		self.ai_settings = ai_settings
 
 		#Load the ship image and get its rect.
 
@@ -15,6 +16,30 @@ class Ship():
 		# Start each new ship at the bottom center of the screen.
 		self.rect.centerx = self.screen_rect.centerx
 		self.rect.bottom = self.screen_rect.bottom
+
+		# Store a deciman value for the ship's center
+		self.center = float(self.rect.centerx)
+
+		# Movement flags
+
+		self.moving_right = False
+		self.moving_left = False
+
+	def update(self):
+		"""Update the ship's position based on the movement flags"""
+		# Upfate the ship's center value, not the rect.
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			# self.rect.centerx += 1
+			self.center += self.ai_settings.ship_speed_factor
+
+		if self.moving_left and self.rect.left > 0:
+			# self.rect.centerx -= 1
+			self.center -= self.ai_settings.ship_speed_factor
+
+		# Update rect object from self.center
+		self.rect.centerx = self.center
+
+
 
 	def blitme(self):
 		"""Draw the ship at its current location"""
